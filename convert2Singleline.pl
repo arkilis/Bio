ed29f4a61664927345f9d281c99d34993f68c687
Ben <arkilis@gmail.com>
2014-12-09 15:14:28 +1000

    Update and rename singleline.pl to convert2Singleline.pl

--- convert2Singleline.pl
+++ convert2Singleline.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl -w
-#perl fa2oneline.pl sample.fa > out.fa
+# USAGE: perl fa2oneline.pl sample.fa > out.fa
 # downloaded from http://www.bioinformatics-made-simple.com
 # change multiline fasta to single line fasta
+# 
 use strict;
 
 my $input_fasta=$ARGV[0];
@@ -12,9 +13,9 @@ print $line;
 
 while ($line = <IN>)
 {
-chomp $line;
-if ($line=~m/^>/) { print "\n",$line,"\n"; }
-else { print $line; }
+  chomp $line;
+  if ($line=~m/^>/) { print "\n",$line,"\n"; }
+  else { print $line; }
 }
 
 print "\n";
